495dc7d5b46b3c13dbb1c34c9d64bef517a813ba
Bima1301 <bima.aji1380@gmail.com>
2024-06-16 14:08:32 +0700

    [FEAT] Done trading video section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,32 +1,39 @@
-import { View, Text, FlatList, Image, RefreshControl } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import { getAllPosts, getLatestPosts } from "../../lib/appwirte";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPost } = useAppwrite(getLatestPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {
+  const onRefresh = async () => {
     setRefreshing(true);
-
+    await refetch();
     setRefreshing(false);
   };
 
   return (
     <SafeAreaView className="bg-primary border-2 h-full">
       <FlatList
-        data={[
-          { id: "1", title: "Item 1" },
-          { id: "2", title: "Item 2" },
-          { id: "3", title: "Item 3" },
-        ]}
+        data={posts}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl">{item.title}</Text>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
@@ -51,15 +58,7 @@ const Home = () => {
                 Latest Videos
               </Text>
 
-              <Trending
-                posts={
-                  [
-                    { id: 1, title: "Post 1" },
-                    { id: 2, title: "Post 2" },
-                    { id: 3, title: "Post 3" },
-                  ] ?? []
-                }
-              />
+              <Trending posts={latestPost} />
             </View>
           </View>
         )}
@@ -69,7 +68,9 @@ const Home = () => {
             subtitle="Be the first to post a video!"
           />
         }
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
